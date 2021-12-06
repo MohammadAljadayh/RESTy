9d128957e863876fa2a0d5ba13608d9c5f816602
MohammadAljadayh <mjedayh@gmail.com>
2021-12-06 17:18:39 +0200

    lab27

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
+import LoadingSpinner from "../loading/loading";
+
 function Results(props) {
   return (
     <section>
-    <pre>{props.data ? <JSONPretty data={props.data} /> : null}</pre>
+    <pre>{props.data ? <JSONPretty data={props.data} /> : <LoadingSpinner />}</pre>
     </section>
   );
 }
