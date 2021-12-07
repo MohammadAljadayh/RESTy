4a80261ff003bcdf53b95244ac1c303071d3887f
MohammadAljadayh <mjedayh@gmail.com>
2021-12-07 16:14:57 +0200

    Updated

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ### Deployment Test
 - [SandBoxlab27](https://enoo7.csb.app/) 
 - [pull request 3 ](https://github.com/MohammadAljadayh/RESTy/pull/3).
-- [pull request 4 ](https://github.com/MohammadAljadayh/RESTy/pull/3).
+- [pull request 4 ](https://github.com/MohammadAljadayh/RESTy/pull/4).
 - [Main Repo link](https://github.com/MohammadAljadayh/RESTy) 
 
 
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./form.scss";
-import { useState } from 'react';
+import { useState ,useEffect } from 'react';
 
 function Form(props) {
 
-  const [method, setMethod] = useState('get');  // for methode
+  const [method, setMethod] = useState('post');  // for methode
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon'); //For Url
   const [view, setView] = useState(false); // for view json file
   const [request, setrequest] = useState('');
+    
+  const colors = {
+    Sea: '#a2ccb6',
+    Sand: '#fceeb5',
+    Peach: '#ee786e',
+  }
+
+  const [color,setColor]=useState(colors.Sea);
+
+  // useEffect(
+  //   () => {
+  //     let v = document.getElementById(`${method}`);
+  //     v.style.background=color;
+  //     // document.body.style.background = color
+  //   },
+  //   [color]
+  // )
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,16 +43,32 @@ function Form(props) {
   const methodH = (e) => {
     setView(false);
     setMethod(e.target.id);
+    setColor('#ee786e');
   }
 
   const textAreaH = (e) => {
     setView(true);
     setMethod(e.target.id);
+    setColor('#ee786e');
   }
 
   const requestH = (e) => {
     setrequest(e.target.value);
+    setColor('#ee786e');
   }
+
+  useEffect(
+    () => {
+      document.getElementById('get').style.background=colors.Sea;
+      document.getElementById('post').style.background=colors.Sea;
+      document.getElementById('put').style.background=colors.Sea;
+      document.getElementById('delete').style.background=colors.Sea;
+      let v = document.getElementById(`${method}`);
+      v.style.background=color;
+    },
+    [method]
+  )
+
 
   return (
     <>
@@ -46,10 +79,10 @@ function Form(props) {
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get" onClick={methodH} >GET </span>
-          <span id="post" onClick={textAreaH} >POST</span>
-          <span id="put" onClick={textAreaH} >PUT</span>
-          <span id="delete" onClick={methodH}>DELETE </span>
+          <span  id="get" onClick={methodH}  >GET </span>
+          <span  id="post" onClick={textAreaH} >POST</span>
+          <span  id="put" onClick={textAreaH} >PUT</span>
+          <span  id="delete" onClick={methodH}>DELETE </span>
         </label>
       </form>
       {view && <textarea rows="6" cols="10" onChange={requestH} />}
